c50acb0474e6c1e70cc0f51c7d8104ad08ea5092
Josnei Luis Olszewski Junior <josneijr@gmail.com>
2019-02-14 22:47:47 -0200

    Completando os métodos necessários na classe principal

--- AgileContent2/Program.cs
+++ AgileContent2/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using AgileContent2.Domains.DataInterpreters;
 using AgileContent2.Domains.DataWriters;
+using AgileContent2.Entities;
 using AgileContent2.Interfaces;
 
 namespace AgileContent2
@@ -11,8 +13,7 @@ namespace AgileContent2
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+            ConvertFile(args[0], args[1]);
         }
 
         public static void ConvertFile(string sourceUrl, string targetPath)
@@ -20,15 +21,47 @@ namespace AgileContent2
             IDataInterpreter dataInterpreter = new MinhaCDN();
             IDataReformat dataReformat = new Agora();
 
-            DownloadFile(sourceUrl, targetPath);
+            string downloadPath = Path.ChangeExtension(targetPath, "tmp");
+
+            try
+            {
+                //Primeiro, pegamos o arquivo da origem (download)
+                DownloadFile(sourceUrl, downloadPath);
+
+                //Vamos agora abrir o arquivo e deixá-lo em memória
+                string fileContent = OpenFileContent(targetPath);
+
+                //Vamos interpretar o arquivo, pegando todos os eventos que encontrarmos
+                List<DataEvent> dataEvents = dataInterpreter.InterpretData(fileContent);
+
+                //E com os eventos, geramos a saída no formato que queremos
+                string output = dataReformat.ReformatData(dataEvents);
+
+                //Por último, flush nos dados
+                SaveBufferToFile(output);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro ao converter dados: " + e.Message);
+            }
+        }
+
+        private static void SaveBufferToFile(string output)
+        {
+
         }
 
         public static void DownloadFile(string sourceUrl, string outputPath)
         {
             using (var client = new WebClient())
             {
-                client.DownloadFile(sourceUrl, Path.ChangeExtension(outputPath, "tmp"));
+                client.DownloadFile(sourceUrl, outputPath);
             }
         } 
+
+        public static string OpenFileContent(string filePath)
+        {
+            return "";
+        }
     }
 }
