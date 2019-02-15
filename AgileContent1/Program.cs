8423496ac377dd5878f5c0250e4ed9c69fe6f310
Josnei Luis Olszewski Junior <josneijr@gmail.com>
2019-02-14 23:34:33 -0200

    Tudo pronto :)

--- AgileContent1/Program.cs
+++ AgileContent1/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Linq;
 
 namespace AgileContent1
 {
+    public class Solution
+    {
+        public int solution(int number)
+        {
+            //Iniciamos o array zerado
+            int[] digitsCount = Enumerable.Repeat(0, 10).ToArray();
+
+            //Vamos descobrir quanto de cada dígito (0-9) temos no número
+            while (number > 0)
+            {
+                digitsCount[number % 10]++;
+                number = number / 10;
+            }
+
+            //O número será formado pelos dígitos encontrados, em ordem decrescente
+            for (int i = 9; i >= 0; i--)
+            {
+                while (digitsCount[i] != 0)
+                {
+                    number = number * 10 + i;
+                    digitsCount[i]--;
+                }
+            }
+
+            if (number > 100000000)
+            {
+                return -1;
+            }
+
+            return number;
+        }
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
---
+++ AgileContent1_Test/Test.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using AgileContent1;
+
+namespace AgileContent1_Test
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase_321()
+        {
+            Solution solution = new Solution();
+
+            Assert.AreEqual(321, solution.solution(123));
+        }
+
+        [Test()]
+        public void TestCase_56998123()
+        {
+            Solution solution = new Solution();
+
+            Assert.AreEqual(99865321, solution.solution(56998123));
+        }
+
+        [Test()]
+        public void TestCase_9999999999()
+        {
+            Solution solution = new Solution();
+
+            Assert.AreEqual(-1, solution.solution(999999999));
+        }
+    }
+}
